70200cf0b1cca7226a3601d8989001e9fca378bf
Christoph Reiter <reiter.christoph@gmail.com>
2016-08-15 16:13:11 +0200

    Try to add travis support for Linux and Windows

---
+++ run_wine.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# ./run_wine.sh 2.7.11 setup.py test
+# ./run_wine.sh 3.4.4 setup.py test
+
+DIR=$(mktemp -d)
+export WINEPREFIX="$DIR/_wine_env"
+export WINEDLLOVERRIDES="mscoree,mshtml="
+export WINEDEBUG="-all"
+mkdir -p "$WINEPREFIX"
+
+VERSION="$1"
+TEMP=${VERSION//./}
+DIRNAME="Python"${TEMP:0:2}
+
+wget -P "$DIR" -c "https://www.python.org/ftp/python/$VERSION/python-$VERSION.msi"
+wine msiexec /a "$DIR/python-$VERSION.msi" /qb
+
+PYTHONEXE="$WINEPREFIX/drive_c/$DIRNAME/python.exe"
+wine "$PYTHONEXE" -m pip install pytest
+wine "$PYTHONEXE" ${@:2}
+exit_code=$?
+wineserver --wait
+rm -Rf "$DIR"
+exit $exit_code
