6df316034d5159e9cad4c0a9a790af9d8f7f2523
Christoph Reiter <reiter.christoph@gmail.com>
2016-08-20 14:23:18 +0200

    print: decide based on the fd if we should bypass the file object
    This makes it work with active colorama which monkey patches
    sys.stdout/stderr

--- senf/_print.py
+++ senf/_print.py
@@ -48,7 +48,7 @@ def print_(*objects, **kwargs):
     if end == "\n":
         end = os.linesep
 
-    if os.name == "nt" and file in (sys.__stdout__, sys.__stderr__):
+    if os.name == "nt":
         _print_windows(objects, sep, end, file, flush)
     else:
         _print_default(objects, sep, end, file, flush)
@@ -105,12 +105,12 @@ ansi_state = AnsiState()
 def _print_windows(objects, sep, end, file, flush):
     """The windows implementation of print_()"""
 
-    if file is sys.__stdout__:
+    fileno = file.fileno()
+    h = winapi.INVALID_HANDLE_VALUE
+    if fileno == 1:
         h = winapi.GetStdHandle(winapi.STD_OUTPUT_HANDLE)
-    elif file is sys.__stderr__:
+    elif fileno == 2:
         h = winapi.GetStdHandle(winapi.STD_ERROR_HANDLE)
-    else:
-        assert 0
 
     if h == winapi.INVALID_HANDLE_VALUE:
         return _print_default(objects, sep, end, file, flush)
@@ -137,8 +137,6 @@ def _print_windows(objects, sep, end, file, flush):
 
     # make sure we flush before we apply any console attributes
     file.flush()
-
-    fileno = file.fileno()
 
     # try to force a utf-8 code page
     old_cp = winapi.GetConsoleOutputCP()
