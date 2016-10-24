5ef5ceaeb0b91eff94af5a388742b9ee06ab6fbb
Christoph Reiter <reiter.christoph@gmail.com>
2016-10-24 19:36:34 +0200

    run_wine: remove get-pip. was only needed for py2.6

--- run_wine.sh
+++ run_wine.sh
@@ -19,9 +19,6 @@ wine msiexec /a "$DIR/python-$VERSION.msi" /qb
 
 PYTHONEXE="$WINEPREFIX/drive_c/$DIRNAME/python.exe"
 PIPEXE="$WINEPREFIX/drive_c/$DIRNAME/Scripts/pip.exe"
-wget "https://bootstrap.pypa.io/get-pip.py"
-wine "$PYTHONEXE" get-pip.py
-rm get-pip.py
 wine "$PIPEXE" install pytest coverage hypothesis
 
 if [ "$2" == "cmd" ]; then
