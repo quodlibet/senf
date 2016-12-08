d02a6c29182eed6ea8724557bb95c3d49750a85f
Christoph Reiter <reiter.christoph@gmail.com>
2016-12-08 14:22:18 +0100

    run_wine: support Python 3.5.2

--- run_wine.sh
+++ run_wine.sh
@@ -3,6 +3,7 @@
 # ./run_wine.sh 2.7.12 cmd
 # ./run_wine.sh 3.4.4 python
 # ./run_wine.sh 3.4.4 cmd
+# ./run_wine.sh 3.5.2 cmd
 
 SCRIPTDIR="$( cd "$( dirname "$0" )" && pwd )"
 DIR=$(mktemp -d)
@@ -13,13 +14,26 @@ mkdir -p "$WINEPREFIX"
 
 VERSION="$1"
 TEMP=${VERSION//./}
-DIRNAME="Python"${TEMP:0:2}
+PYVER=${TEMP:0:2}
+DIRNAME="Python"${PYVER}
+DESTDIR="$WINEPREFIX/drive_c/$DIRNAME"
 
-wget -P "$SCRIPTDIR/.cache" -c "https://www.python.org/ftp/python/$VERSION/python-$VERSION.msi"
-wine msiexec /a "$SCRIPTDIR/.cache/python-$VERSION.msi" /qb
+if [ "${PYVER}" = "35" ] ; then
+    wget -P "$DIR" "https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks"
+    chmod a+x "$DIR/winetricks"
+    "$DIR/winetricks" -q vcrun2015
+    wget -P "$SCRIPTDIR/.cache" -c "https://www.python.org/ftp/python/${VERSION}/python-${VERSION}-embed-win32.zip"
+    unzip "$SCRIPTDIR/.cache/python-${VERSION}-embed-win32.zip" -d "$DESTDIR"
+    unzip "$DESTDIR/python$PYVER.zip" -d "$DESTDIR/Lib"
+    rm -Rf "$DESTDIR/python$PYVER.zip"
+    wine reg add "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment" /v Path /t REG_SZ /d "%path%;$(winepath $DESTDIR);$(winepath $DESTDIR/Scripts)"
+else
+    wget -P "$SCRIPTDIR/.cache" -c "https://www.python.org/ftp/python/$VERSION/python-$VERSION.msi"
+    wine msiexec /a "$SCRIPTDIR/.cache/python-$VERSION.msi" /qb
+fi
 
-PYTHONEXE="$WINEPREFIX/drive_c/$DIRNAME/python.exe"
-PIPEXE="$WINEPREFIX/drive_c/$DIRNAME/Scripts/pip.exe"
+PYTHONEXE="$DESTDIR/python.exe"
+PIPEXE="$DESTDIR/Scripts/pip.exe"
 wget "https://bootstrap.pypa.io/get-pip.py"
 wine "$PYTHONEXE" get-pip.py
 rm get-pip.py
