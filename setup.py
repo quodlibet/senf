a672c9071449673c4bb04f6dc8fbc521506f5f7f
Christoph Reiter <reiter.christoph@gmail.com>
2019-11-13 23:52:14 +0100

    setup.py: add python_requires

--- setup.py
+++ setup.py
@@ -120,11 +120,14 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: Implementation :: CPython',
             'Programming Language :: Python :: Implementation :: PyPy',
             'License :: OSI Approved :: MIT License',
         ],
         tests_require=['pytest'],
+        python_requires=(
+            '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4'),
         cmdclass={
             'test': pytest_command,
             'coverage': coverage_command,
